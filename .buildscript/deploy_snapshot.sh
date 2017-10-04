269fbd8d660a6a4fbff92872cda94b6afc5962b7
Andrew Cranston <andrew.cranston@gmail.com>
2017-10-04 15:57:22 +1100

    Fix snapshot push

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -21,7 +21,7 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew uploadArchives
+  ./gradlew uploadArchives --debug --stacktrace
   echo "Snapshot deployed!"
 fi
 
