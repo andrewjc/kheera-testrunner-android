8cc592f1a5d50f49d7db9fef6d703541a2d8c9cf
Andrew Cranston <andrew.cranston@auspost.com.au>
2017-10-13 14:41:52 +1100

    Add some support for future android studio plugin work.

--- test-runner/src/main/java/com/kheera/annotations/TestModuleAnnotation.java
+++ test-runner/src/main/java/com/kheera/annotations/TestModuleAnnotation.java
@@ -6,8 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@TestModuleAnnotation
-public @interface TestModule {
-    String featureFile();
+@Target({ElementType.TYPE})
+public @interface TestModuleAnnotation {
 }
